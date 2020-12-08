de4954489cf73419dc1b440d427757ae75e11e0e
Jairo Cuadra <jairocuadra@Jairos-MacBook-Pro.local>
2020-12-07 19:51:06 -0500

    starting project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A  test Recipe',
+      'This is simply a test',
+      'https://www.simplyrecipes.com/wp-content/uploads/2006/08/HT-Cut-A-Mango-LEAD-1.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
