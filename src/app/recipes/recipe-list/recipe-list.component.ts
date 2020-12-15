239708941229e4f7042cbc9c28c647a07058348c
Jairo Cuadra <jairo_cuadra@outlook.com>
2020-12-15 17:32:10 -0500

    header toggle, recipe list toggle

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,20 +7,25 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
-      'A  test Recipe',
-      'This is simply a test',
+      'Mango Recipe',
+      'Just a name I made up for the purposes of this app',
       'https://www.simplyrecipes.com/wp-content/uploads/2006/08/HT-Cut-A-Mango-LEAD-1.jpg'
     ),
     new Recipe(
-      'A  test Recipe',
-      'This is simply a test',
-      'https://www.simplyrecipes.com/wp-content/uploads/2006/08/HT-Cut-A-Mango-LEAD-1.jpg'
+      'Pineapple Recipe',
+      'Another funny name I made up. Maybe Ill add some actual dishes here',
+      'https://i5.walmartimages.com/asr/bc5da1a1-6b40-4233-9abd-1126475d2864_9.0b874251fccc645fd98ac76e797c2d2a.jpeg'
     ),
   ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
